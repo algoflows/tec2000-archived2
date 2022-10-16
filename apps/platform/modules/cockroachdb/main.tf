031d78676b77a696f76f679ecc39be46645536ce
Sean Knowles <s@seanknowles.dev>
2022-10-16 20:32:18 +0100

    feat: added cokcroachdb resource

--- apps/platform/modules/cockroachdb/main.tf
+++ apps/platform/modules/cockroachdb/main.tf
@@ -5,3 +5,12 @@ terraform {
     }
   }
 }
+
+resource "cockroach_cluster" "cluster" {
+  name             = "tec2000"
+  cluster_provider = "gcp"
+  serverless {
+    region      = "europe-west1"
+    spend_limit = 100
+  }
+}
