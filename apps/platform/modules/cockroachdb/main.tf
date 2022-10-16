5bbe5c85c11ffb9c9d07ba5abf9f99b95114a1b5
Sean Knowles <s@seanknowles.dev>
2022-10-16 20:35:13 +0100

    fix: removed cockroach provider

--- apps/platform/modules/cockroachdb/main.tf
+++ apps/platform/modules/cockroachdb/main.tf
@@ -1,16 +1,16 @@
-terraform {
-  required_providers {
-    cockroachdb = {
-      source = "aybabtme/cockroach"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     cockroachdb = {
+#       source = "aybabtme/cockroach"
+#     }
+#   }
+# }
 
-resource "cockroach_cluster" "cluster" {
-  name             = "tec2000"
-  cluster_provider = "gcp"
-  serverless {
-    region      = "europe-west1"
-    spend_limit = 100
-  }
-}
+# resource "cockroach_cluster" "cluster" {
+#   name             = "tec2000"
+#   cluster_provider = "gcp"
+#   serverless {
+#     region      = "europe-west1"
+#     spend_limit = 100
+#   }
+# }
