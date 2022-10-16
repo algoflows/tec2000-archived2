e44f99177107de8e850945c1ad065263920574d6
Sean Knowles <s@seanknowles.dev>
2022-10-16 18:13:37 +0100

    fix: testing deploy by adding to next-web

--- apps/next-web/pages/index.tsx
+++ apps/next-web/pages/index.tsx
@@ -4,7 +4,12 @@ export function Index() {
    *
    * Note: The corresponding styles are in the ./index.css file.
    */
-  return <h1 className="bg-indigo-500 p-2 font-mono">Welcome next-web</h1>;
+  return (
+    <>
+      <h1 className="bg-indigo-500 p-2 font-mono">Welcome next-web</h1>
+      <p className="p-2">Lets talk about the future of web development</p>
+    </>
+  );
 }
 
 export default Index;
